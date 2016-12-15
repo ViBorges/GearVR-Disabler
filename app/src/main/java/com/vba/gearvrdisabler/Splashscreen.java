ef95a9d13e03a5dc5d78d838eb53a82eb59dacab
ViBorges <vbda123@gmail.com>
2016-12-15 21:35:11 -0200

    splashscreen

---
+++ app/src/main/java/com/vba/gearvrdisabler/Splashscreen.java
@@ -0,0 +1,18 @@
+package com.vba.gearvrdisabler;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
