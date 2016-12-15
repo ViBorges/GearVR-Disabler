cb5ce7dc0a0a591f8b74ef1b6d44088256c115dc
ViBorges <vbda123@gmail.com>
2016-12-15 15:11:01 -0200

    Icon, color and admin acess classes created

---
+++ app/src/main/java/com/vba/gearvrdisabler/AdminVerification.java
@@ -0,0 +1,11 @@
+package com.vba.gearvrdisabler;
+
+public class AdminVerification extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+    super.onCreate();
+
+
+    }
+}
---
+++ app/src/main/java/com/vba/gearvrdisabler/GrantAdminAcess.java
@@ -0,0 +1,9 @@
+package com.vba.gearvrdisabler;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GrantAdminAcess extends DeviceAdminReceiver {
+
+}
